da5f359988ed432383f59cda6d076140e66b0542
Ryan Yogan <ryanyogan@gmail.com>
2024-02-16 02:33:47 -0600

    use system font on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,7 @@ import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ryan Yogan",
@@ -19,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Header />
         {children}
         <Footer />
